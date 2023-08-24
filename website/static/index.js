eb79034f5d71963818e1b538740a9cc6ad46c137
Rehaan12345 <rehaan1099@gmail.com>
2023-08-23 20:30:33 -0400

    Added the inital dropdown PROFILE button. Still more work to be needed but the initial processes are finished.

--- website/static/index.js
+++ website/static/index.js
@@ -175,3 +175,30 @@ window.addEventListener("DOMContentLoaded", () => {
     // Applies the correct theme: 
     retrieveTheme();
 });
+
+// ********** Nav button functionality: ********** //
+// window.addEventListener("DOMContentLoaded", () => {
+//     const navElement = document.querySelector(".navlink");
+//     console.log("Target: " + navElement)
+//     navElement.addEventListener("click", () => {
+//         navElement.classList.add("activeclick");
+//     });
+// });
+
+window.addEventListener("DOMContentLoaded", ()=> {
+    document.addEventListener("click", e=> {
+        const isDropDownButton = e.target.matches("[data-dropdown-button]");
+        if(!isDropDownButton && e.target.closest("[data-dropdown]") != null) return;
+
+        let currentDropDown;
+        if(isDropDownButton) {
+            currentDropDown = e.target.closest("[data-dropdown]");
+            currentDropDown.classList.toggle("active");
+        }
+        
+        document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+            if (dropdown === currentDropDown) return;
+            dropdown.classList.remove("active");
+        });
+    });
+});
