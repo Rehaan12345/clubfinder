03b3cb31c160a6fc64b765fe27b885e4089a5739
Rehaan12345 <rehaan1099@gmail.com>
2023-11-12 10:28:55 -0500

    heroku

--- website/__init__.py
+++ website/__init__.py
@@ -17,7 +17,7 @@ admin = Admin()
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "rehaan"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://tiblksieyrwpzj:ff96a788168ef8629773d837f23986697f79bbc4877fae8ff66a024dee1e424c@ec2-44-214-132-149.compute-1.amazonaws.com:5432/ddcf3bepqp0a9"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://jlfpuxzdqrkneh:f3ca613563b5efcad1f6c485e7a27118b7cd0d78c2326a70ee98fb21d9f4c248@ec2-52-0-79-72.compute-1.amazonaws.com:5432/ddmnvas8sg8evg"
     # app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}" 
     db.init_app(app) 
     # Configuring the mail messaging system:
